7594c8fcc8b4b8e3fc5354fc1f2931f829506c29
Cosmin Stejerean <cosmin@offbytwo.com>
2012-11-25 21:49:10 -0600

    add support for fetching build parameters

--- src/main/java/com/offbytwo/jenkins/model/BuildWithDetails.java
+++ src/main/java/com/offbytwo/jenkins/model/BuildWithDetails.java
@@ -6,7 +6,18 @@
 
 package com.offbytwo.jenkins.model;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Lists;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.google.common.collect.Collections2.filter;
+
 public class BuildWithDetails extends Build {
+    List actions;
     boolean building;
     int duration;
     String fullDisplayName;
@@ -18,47 +29,48 @@ public class BuildWithDetails extends Build {
         return building;
     }
 
-    public void setBuilding(boolean building) {
-        this.building = building;
-    }
-
     public int getDuration() {
         return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
     }
 
     public String getFullDisplayName() {
         return fullDisplayName;
     }
 
-    public void setFullDisplayName(String fullDisplayName) {
-        this.fullDisplayName = fullDisplayName;
-    }
-
     public String getId() {
         return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public long getTimestamp() {
         return timestamp;
     }
 
-    public void setTimestamp(long timestamp) {
-        this.timestamp = timestamp;
-    }
-
     public BuildResult getResult() {
         return result;
     }
 
-    public void setResult(BuildResult result) {
-        this.result = result;
+    public void setActions(List actions) {
+        this.actions = actions;
+    }
+
+    public Map<String, String> getParameters() {
+        Collection parameters = filter(actions, new Predicate<Map<String, Object>>() {
+            @Override
+            public boolean apply(Map<String, Object> action) {
+                return action.containsKey("parameters");
+            }
+        });
+
+        Map<String, String> params = new HashMap<String, String>();
+
+        if (parameters != null && !parameters.isEmpty()) {
+            for(Map<String, String> param : ((Map<String, List<Map<String, String>>>) parameters.toArray()[0]).get("parameters")) {
+                String key = param.get("name");
+                String value = param.get("value");
+                params.put(key, value);
+            }
+        }
+
+        return params;
     }
 }
