ba3847700a3e8b71f6e05bab5a67ecea2a6a2a1a
kellyp <kellyplummer@mac.com>
2013-08-12 14:56:54 -0700

    adding interface for labels and computers

---
+++ src/main/java/com/offbytwo/jenkins/model/Computer.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2012 Cosmin Stejerean.
+ *
+ * Distributed under the MIT license: http://opensource.org/licenses/MIT
+ */
+
+package com.offbytwo.jenkins.model;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static java.net.URLEncoder.encode;
+import static org.apache.commons.lang.StringUtils.join;
+
+public class Computer extends BaseModel {
+    private String displayName;
+
+    public List<Computer> getComputers() {
+        return computer;
+    }
+
+    public void setComputer(List<Computer> computer) {
+        this.computer = computer;
+    }
+
+    List<Computer> computer;
+
+    public Computer() {
+    }
+
+    public Computer(String displayName) {
+        this();
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return this.displayName;
+    }
+
+    public ComputerWithDetails details() throws IOException {
+        return client.get("/computer/" + displayName.replaceAll("master", "(master)"), ComputerWithDetails.class);
+    }
+
+    private static class MapEntryToQueryStringPair implements Function<Map.Entry<String, String>, String> {
+        @Override
+        public String apply(Map.Entry<String, String> entry) {
+            return encode(entry.getKey()) + "=" + encode(entry.getValue());
+        }
+    }
+}
