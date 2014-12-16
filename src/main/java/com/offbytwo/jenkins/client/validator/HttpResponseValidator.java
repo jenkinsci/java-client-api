19a9bd9f76e170c0845b7c8f1e05746ade486eab
ufna <ufna@ufna.ru>
2014-12-16 09:48:29 +0300

    Response Status debug

--- src/main/java/com/offbytwo/jenkins/client/validator/HttpResponseValidator.java
+++ src/main/java/com/offbytwo/jenkins/client/validator/HttpResponseValidator.java
@@ -8,6 +8,7 @@ public class HttpResponseValidator {
     public void validateResponse(HttpResponse response) throws HttpResponseException {
         int status = response.getStatusLine().getStatusCode();
         if (status < 200 || status >= 400) {
+            System.out.println("Unknown response code: " + status);
             throw new HttpResponseException(status, response.getStatusLine().getReasonPhrase());
         }
     }
