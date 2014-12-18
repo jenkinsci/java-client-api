80430e822f70226957c20b7ced80b038791db519
Vladimir Alyamkin <ufna@ufna.ru>
2014-12-18 05:33:03 +0300

    File parameters

--- src/main/java/com/offbytwo/jenkins/model/Job.java
+++ src/main/java/com/offbytwo/jenkins/model/Job.java
@@ -9,12 +9,16 @@ package com.offbytwo.jenkins.model;
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
 import com.offbytwo.jenkins.client.JenkinsPostResult;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
 import static java.net.URLEncoder.encode;
 import static org.apache.commons.lang.StringUtils.join;
+import static org.apache.commons.lang.StringUtils.remove;
 
 public class Job extends BaseModel {
 
@@ -44,8 +48,17 @@ public class Job extends BaseModel {
     /**
      * Trigger a build without parameters
      */
-    public JenkinsPostResult build() throws IOException {
-        return client.post(url + "build");
+    public Build build() throws IOException {
+        JenkinsPostResult result = client.post(url + "build");
+
+        // Construct build from url location
+        Object location = result.getFirstHeader("Location");
+        if (location == null || location.toString() == null || location.toString().isEmpty()) {
+            return null;
+        }
+
+        // @TODO Isolate build number from url
+        return new Build(-1, location.toString());
     }
 
     /**
@@ -54,9 +67,18 @@ public class Job extends BaseModel {
      * @param params the job parameters
      * @throws IOException
      */
-    public JenkinsPostResult build(Map<String, String> params) throws IOException {
+    public Build build(Map<String, String> params, Map<String, File> fileParams) throws IOException {
         String qs = join(Collections2.transform(params.entrySet(), new MapEntryToQueryStringPair()), "&");
-        return client.post(url + "buildWithParameters?" + qs, null, null);
+        JenkinsPostResult result = client.post(url + "buildWithParameters?" + qs, null, null, fileParams);
+
+        // Construct build from url location
+        Object location = result.getFirstHeader("Location");
+        if (location == null || location.toString() == null || location.toString().isEmpty()) {
+            return null;
+        }
+
+        // @TODO Isolate build number from url
+        return new Build(-1, location.toString());
     }
 
     private static class MapEntryToQueryStringPair implements Function<Map.Entry<String, String>, String> {
