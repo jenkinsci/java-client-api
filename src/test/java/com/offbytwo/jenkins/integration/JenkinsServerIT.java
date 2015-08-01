5b84e1597f43414bbe7dc2ca9d9d8196dae43f2a
Karl Heinz Marbaise <khmarbaise@apache.org>
2015-08-01 18:37:10 +0200

    - Reduced pom configuration by following   naming conventions of maven-surefire-plugin   and maven-failsafe-plugin. - Removed WARNINGs during maven build by correctly defining plugin versions. - Upgrade maven-jar-plugin to non ancient version.

--- src/test/java/com/offbytwo/jenkins/integration/JenkinsServerIT.java
+++ src/test/java/com/offbytwo/jenkins/integration/JenkinsServerIT.java
@@ -29,7 +29,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-public class JenkinsServerIntegrationTest {
+public class JenkinsServerIT {
 
     @Rule
     public JenkinsRule jenkinsRule = new JenkinsRule();
