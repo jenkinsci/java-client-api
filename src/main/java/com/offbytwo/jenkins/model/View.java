657e23a47f124fb30b046a532450998bcd06201a
Karl Heinz Marbaise <khmarbaise@apache.org>
2016-04-03 14:30:09 +0200

    Fixed #130  o Changed the scope of org.jvnet.hudson:xstream    to test cause it is used only during the tests.  o I needed to remove a toString method from View class    which uses the XStream classes which does not make sense    from my point of view.

--- src/main/java/com/offbytwo/jenkins/model/View.java
+++ src/main/java/com/offbytwo/jenkins/model/View.java
@@ -1,8 +1,5 @@
 package com.offbytwo.jenkins.model;
 
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.json.JsonHierarchicalStreamDriver;
-
 public class View extends MainView {
 
 	private String name = "";
@@ -37,34 +34,48 @@ public class View extends MainView {
 		this.url = url;
 	}
 
-	@Override
-	public String toString() {
-		XStream xstream = new XStream(new JsonHierarchicalStreamDriver());
-		return xstream.toXML(this);
+    @Override
+	public boolean equals( Object obj )
+	{
+		if ( this == obj )
+			return true;
+		if ( obj == null )
+			return false;
+		if ( getClass() != obj.getClass() )
+			return false;
+		View other = (View) obj;
+		if ( description == null )
+		{
+			if ( other.description != null )
+				return false;
+		}
+		else if ( !description.equals( other.description ) )
+			return false;
+		if ( name == null )
+		{
+			if ( other.name != null )
+				return false;
+		}
+		else if ( !name.equals( other.name ) )
+			return false;
+		if ( url == null )
+		{
+			if ( other.url != null )
+				return false;
+		}
+		else if ( !url.equals( other.url ) )
+			return false;
+		return true;
 	}
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        View view = (View) o;
-
-        if (description != null ? !description.equals(view.description) : view.description != null)
-            return false;
-        if (name != null ? !name.equals(view.name) : view.name != null)
-            return false;
-        if (url != null ? !url.equals(view.url) : view.url != null)
-            return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        result = 31 * result + (url != null ? url.hashCode() : 0);
-        return result;
-    }
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ( ( description == null ) ? 0 : description.hashCode() );
+		result = prime * result + ( ( name == null ) ? 0 : name.hashCode() );
+		result = prime * result + ( ( url == null ) ? 0 : url.hashCode() );
+		return result;
+	}
 }
