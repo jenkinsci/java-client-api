4e4dbed54f2f5479e275ec34debb507187989de6
Karl Heinz Marbaise <khmarbaise@apache.org>
2016-05-21 01:53:30 +0200

    Changed the output layout for the IT's

--- jenkins-client-it-docker/src/test/java/com/offbytwo/jenkins/integration/MethodListener.java
+++ jenkins-client-it-docker/src/test/java/com/offbytwo/jenkins/integration/MethodListener.java
@@ -6,35 +6,26 @@ import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 import org.testng.SkipException;
 
-public class MethodListener
-    implements IInvokedMethodListener
-{
-    
+public class MethodListener implements IInvokedMethodListener {
+
     private String lastClassWithoutPackage;
 
-    private String getClassNameWithoutPackage( String canonicalName )
-    {
-        int lastIndexOf = canonicalName.lastIndexOf( '.' );
-        return canonicalName.substring( lastIndexOf + 1 );
+    private String getClassNameWithoutPackage(String canonicalName) {
+        int lastIndexOf = canonicalName.lastIndexOf('.');
+        return canonicalName.substring(lastIndexOf + 1);
     }
 
     @Override
-    public void beforeInvocation( IInvokedMethod method, ITestResult testResult )
-    {
-        if ( method.isTestMethod() )
-        {
+    public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+        if (method.isTestMethod()) {
             ITestNGMethod testMethod = method.getTestMethod();
-            String classNameWithoutPackage = getClassNameWithoutPackage( testMethod.getTestClass().getName() );
-            
+            String classNameWithoutPackage = getClassNameWithoutPackage(testMethod.getTestClass().getName());
+
             if (lastClassWithoutPackage == null) {
-                System.out.println( classNameWithoutPackage + ":" );
-                System.out.print( "  " + testMethod.getMethodName() + "..."  );
+                System.out.println(classNameWithoutPackage + ":");
             } else {
-                if (lastClassWithoutPackage.equals( classNameWithoutPackage )) {
-                    System.out.print( "  " + testMethod.getMethodName() + "..."  );
-                } else {
-                    System.out.println( classNameWithoutPackage + ":" );
-                    System.out.print( "  " + testMethod.getMethodName() + "..."  );
+                if (!lastClassWithoutPackage.equals(classNameWithoutPackage)) {
+                    System.out.println(classNameWithoutPackage + ":");
                 }
             }
             lastClassWithoutPackage = classNameWithoutPackage;
@@ -42,23 +33,25 @@ public class MethodListener
     }
 
     @Override
-    public void afterInvocation( IInvokedMethod method, ITestResult testResult )
-    {
-        if ( method.isTestMethod() )
-        {
-            if ( testResult.isSuccess() )
-            {
-                System.out.println( "Done." );
-            }
-            else
-            {
-                //Isn't there an more elegant way?
+    public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+        if (method.isTestMethod()) {
+            ITestNGMethod testMethod = method.getTestMethod();
+            String classNameWithoutPackage = getClassNameWithoutPackage(testMethod.getTestClass().getName());
+
+            if (testResult.isSuccess()) {
+                System.out.print("  SUCCESS ");
+            } else {
+                // Isn't there an more elegant way?
                 if (testResult.getThrowable() instanceof SkipException) {
-                    System.out.println( "SKIPPED. ( " + testResult.getThrowable().getMessage() + " )" );
+                    System.out.print("  SKIPPED ( " + testResult.getThrowable().getMessage() + " ) ");
                 } else {
-                    System.out.println( "FAILURE." );
+                    System.out.print("  FAILURE ");
                 }
             }
+
+            System.out.println("  " + testMethod.getMethodName());
+            lastClassWithoutPackage = classNameWithoutPackage;
+
         }
     }
 
