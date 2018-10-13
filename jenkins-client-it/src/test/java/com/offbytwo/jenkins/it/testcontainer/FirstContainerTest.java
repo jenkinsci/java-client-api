e982752801d0919a82641d5af9a5d944c7e33499
Karl Heinz Marbaise <github@soebes.de>
2018-10-13 13:46:37 +0200

    Fixed FirstContainerTest

--- jenkins-client-it/src/test/java/com/offbytwo/jenkins/it/testcontainer/FirstContainerTest.java
+++ jenkins-client-it/src/test/java/com/offbytwo/jenkins/it/testcontainer/FirstContainerTest.java
@@ -21,50 +21,51 @@ public class FirstContainerTest {
 //	public static GenericContainer<?> jenkins = new GenericContainer("jenkins:1.651.3")
 //			.withExposedPorts(8080);
 //	withCopyFileToContainer(MountableFile.forClasspathResource("/app.jar"), "/")
-	
-	@ClassRule
-	public static GenericContainer<?> jenkins = new GenericContainer(
-			new ImageFromDockerfile()
-    			.withFileFromClasspath("plugins.txt", "/plugins.txt")
-    			.withFileFromClasspath( "config.xml", "/config.xml" )
-				.withDockerfileFromBuilder(builder -> 
-				{
-					builder
-						.from("jenkins:1.651.3")
-						.copy("plugins.txt", "/usr/share/jenkins/ref/plugins.txt")
-						.copy("config.xml", "/usr/share/jenkins/ref/config.xml")
-						.run("/usr/local/bin/plugins.sh", "/usr/share/jenkins/ref/plugins.txt");
-	
-				}));
 
-	
-	@Test
-	public void testName() throws InterruptedException, IOException {
-		System.out.println("Hello world.");
-	
-		String containerIpAddress = jenkins.getContainerIpAddress();
-		Integer mappedPort = jenkins.getMappedPort(8080);
-		System.out.println("IP:" + containerIpAddress + " mappedPort:" + mappedPort);
-		
-		JenkinsServer jenkinsServer = new JenkinsServer(URI.create("http://" + containerIpAddress + ":" + mappedPort + "/"));
-		
-		ToStringConsumer stdoutStringConsumer = new ToStringConsumer();
-		ToStringConsumer stderrStringConsumer = new ToStringConsumer();
-		jenkins.followOutput(stdoutStringConsumer, OutputType.STDOUT);
-		jenkins.followOutput(stderrStringConsumer, OutputType.STDERR);
+    @ClassRule
+    public static GenericContainer<?> jenkinsTestContainer = new GenericContainer<>(
+        new ImageFromDockerfile()
+            .withFileFromClasspath("plugins.txt", "/plugins.txt")
+            .withFileFromClasspath("config.xml", "/config.xml")
+            .withDockerfileFromBuilder(builder ->
+                {
+                    builder
+                        .from("jenkins:1.651.3")
+                        .copy("plugins.txt", "/usr/share/jenkins/ref/plugins.txt")
+                        .copy("config.xml", "/usr/share/jenkins/ref/config.xml")
+                        .run("/usr/local/bin/plugins.sh", "/usr/share/jenkins/ref/plugins.txt");
 
-		String utf8String = stdoutStringConsumer.toUtf8String();
-		String utf8StringErr = stderrStringConsumer.toUtf8String();
+            })
+    );
 
-		System.out.println(utf8String);
-		System.out.println(utf8StringErr);
 
-		while (!jenkinsServer.isRunning()) {
-			Thread.sleep(500);
-		};
-		
-		View job = jenkinsServer.getView( "Test-View" );
-		
-		System.out.println( "Job:" + job.getDescription() + " " + job.getName());
-	}
+    @Test
+    public void testName() throws InterruptedException, IOException {
+        System.out.println("Hello world.");
+
+        String containerIpAddress = jenkinsTestContainer.getContainerIpAddress();
+        Integer mappedPort = jenkinsTestContainer.getMappedPort(8080);
+        System.out.println("IP:" + containerIpAddress + " mappedPort:" + mappedPort);
+
+        JenkinsServer jenkinsServer = new JenkinsServer(URI.create("http://" + containerIpAddress + ":" + mappedPort + "/"));
+
+        ToStringConsumer stdoutStringConsumer = new ToStringConsumer();
+        ToStringConsumer stderrStringConsumer = new ToStringConsumer();
+        jenkinsTestContainer.followOutput(stdoutStringConsumer, OutputType.STDOUT);
+        jenkinsTestContainer.followOutput(stderrStringConsumer, OutputType.STDERR);
+
+        String utf8String = stdoutStringConsumer.toUtf8String();
+        String utf8StringErr = stderrStringConsumer.toUtf8String();
+
+        System.out.println(utf8String);
+        System.out.println(utf8StringErr);
+
+        while (!jenkinsServer.isRunning()) {
+            Thread.sleep(500);
+        }
+
+        View job = jenkinsServer.getView("Test-View");
+
+        System.out.println("Job:" + job.getDescription() + " " + job.getName());
+    }
 }
